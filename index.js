acc2aa9e1671d1597aaa199f5a04c555d108574b
fragya <affc1907@hotmail.com>
2024-05-18 16:36:14 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,7 @@
+document.getElementById('login-btn').addEventListener('click', function() {
+    alert('Login functionality coming soon!');
+});
+
+document.getElementById('connect-wallet-btn').addEventListener('click', function() {
+    alert('Wallet connection functionality coming soon!');
+});
